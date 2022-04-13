b0aaadf71eb1f36a3620b3057e1bd7f2a42a1345
jacky-ui <jacky.du.004@gmail.com>
2022-04-13 08:32:27 -0400

    create responsive navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-
-import './App.css';
+import './App.scss';
+import Navigation from './components/navigation/Navigation.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <h1>Hello World</h1>
+    </>
   );
 }
 
