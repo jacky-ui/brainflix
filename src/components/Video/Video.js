40fd0758c207562ec4c7d8648c1fb499de404f85
jacky-ui <jacky.du.004@gmail.com>
2022-04-16 21:37:50 -0400

    create video, comments and side video with html/css

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -11,7 +11,9 @@ const firstVideo = JsonVideo[0];
 const firstVideoPicture = firstVideo.image;
 const firstVideoTitle = firstVideo.title;
 const firstVideoData = JsonVideoData[0];
+const firstVideoComments = firstVideoData.comments[0];
 console.log(firstVideoData);
+console.log(firstVideoComments);
 
 const formatTime = new Date(firstVideoData.timestamp).toLocaleDateString();
 console.log(formatTime);
@@ -28,6 +30,7 @@ function CreateVideo() {
                 />
             </section>
 
+            <section className="noname">
             <section className="video__details">
                 <h2 className="video__details--title">
                     {firstVideoTitle}
@@ -53,7 +56,7 @@ function CreateVideo() {
                         />
                     </div>
                     </section>
-                    <section>
+                    <section className="video__comment--container">
                         <p className="video__description">{firstVideoData.description}</p>
                         <p className="video__details--bold">3 Comments</p>
 
@@ -61,21 +64,142 @@ function CreateVideo() {
                             <img
                             className="video__comments--profile" 
                             src={ProfileImage}
+                            alt=""
                             />
                             
                             <label className="video__comments--header">JOIN THE CONVERSATION</label>
-                            <input
-                                className="video__comments--textarea" 
-                                type="textarea"
-                                placeholder='Add a new comment'
-                            />
-                            <button type='submit' className="video__comments--submit">COMMENT</button>
-                            <img
-                                className="icon__comment" 
-                                src={CommentIcon}
-                            />
+                            <div className="video__comments--text">
+                                <input
+                                    className="video__comments--textarea" 
+                                    type="textarea"
+                                    placeholder='Add a new comment'
+                                />
+                                <button type='submit' className="video__comments--submit">COMMENT</button>
+                                <img
+                                    className="icon__comment" 
+                                    src={CommentIcon}
+                                    alt=""
+                                />
+                            </div>
                         </div>
                     </section>
+                    <section className="comments">
+                        <div className="comments__container">
+                            <span className="comments__name">Micheal Lyons
+                            <div className="comments__profile"></div>
+                            </span>
+                            <span className="comments__date">08/09/2021</span>
+                        </div>
+                        <p className="comments__paragraph">They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.</p>
+
+                        <div className="comments__container">
+                            <span className="comments__name">Micheal Lyons
+                                <div className="comments__profile"></div>
+                            </span>
+                            <span className="comments__date">08/09/2021</span>
+                        </div>
+                        <p className="comments__paragraph">They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.</p>
+
+                        <div className="comments__container">
+                        <span className="comments__name">Micheal Lyons
+                            <div className="comments__profile"></div>
+                        </span>
+                        <span className="comments__date">08/09/2021</span>
+                        </div>
+                        <p className="comments__paragraph">They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.</p>
+                    </section>
+            </section>
+
+                <section className="side">
+                    <h2 className="side__title">NEXT VIDEOS</h2>
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+
+                    <div className="side__container">
+                        <video 
+                        controls poster={firstVideoPicture}
+                        className="side__video"
+                        />
+                        <div className="side__content">
+                            <span className="side__content--title">Become A Travel Pro In One Easy Lesson</span>
+                            <span className="side__content--uploader">Todd Welch</span>
+                        </div>
+                    </div>
+                </section>
             </section>
         </article>
         </>
